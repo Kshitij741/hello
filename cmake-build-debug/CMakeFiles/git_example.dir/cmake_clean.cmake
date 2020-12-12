eed545cc02874ba8fc0ba0f2dfc5eb7af4c0b197
Kshitij Nikum <kshitijnikum3@gmail.com>
2020-12-12 13:23:17 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/git_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_example.dir/Hello.c.obj"
+  "git_example.exe"
+  "git_example.exe.manifest"
+  "git_example.pdb"
+  "libgit_example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
